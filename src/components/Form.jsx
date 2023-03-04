d8879a9cc5d2008a556a96900d1b855b877bb321
unknown <salamibrahim2003@gmail.com>
2023-03-04 00:35:30 -0400

    First Commit

---
+++ src/components/Form.jsx
@@ -0,0 +1,86 @@
+import { useState, useEffect } from "react";
+import styled from "@emotion/styled";
+import Error from "./Error";
+import useSelectCurrencies from "../hooks/useSelectCurrencies";
+import { currencies } from "../data/currencies";
+
+const InputSubmit = styled.input`
+  background-color: #9497ff;
+  border: none;
+  width: 100%;
+  padding: 10px;
+  color: #fff;
+  font-weight: 700;
+  text-transform: uppercase;
+  font-size: 20px;
+  border-radius: 5px;
+  transition: background-color 0.3s ease;
+  margin-top: 30px;
+  &:hover {
+    background-color: #7a7dfe;
+
+    cursor: pointer;
+  }
+`;
+
+const Form = ({ setCurrencies }) => {
+  const [cryptos, setCryptos] = useState([]);
+  const [error, setError] = useState(false);
+
+  const [currency, SelectCurrencies] = useSelectCurrencies(
+    "Select your currency",
+    currencies
+  );
+  const [crypto, SelectCrypto] = useSelectCurrencies(
+    "Select your crypto",
+    cryptos
+  );
+
+  useEffect(() => {
+    const requestAPI = async () => {
+      const url =
+        "https://min-api.cryptocompare.com/data/top/mktcapfull?limit=10&tsym=USD";
+
+      const answer = await fetch(url);
+      const result = await answer.json();
+
+      const arrayCryptos = result.Data.map((crypto) => {
+        const object = {
+          id: crypto.CoinInfo.Name,
+          name: crypto.CoinInfo.FullName,
+        };
+        return object;
+      });
+
+      setCryptos(arrayCryptos);
+    };
+    requestAPI();
+  }, []);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if ([currency, crypto].includes("")) {
+      setError(true);
+      return;
+    }
+    setError(false);
+    setCurrencies({
+      currency,
+      crypto,
+    });
+  };
+
+  return (
+    <>
+      {error && <Error>Error! Please try again later</Error>}
+      <form onSubmit={handleSubmit}>
+        <SelectCurrencies />
+        <SelectCrypto />
+
+        <InputSubmit type="submit" value="Quote" />
+      </form>
+    </>
+  );
+};
+
+export default Form;
